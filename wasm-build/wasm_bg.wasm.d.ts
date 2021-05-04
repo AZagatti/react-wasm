1f699875d954dae88639de093a427a686acb8fd7
azagatti <andre.zagatti1@gmail.com>
2021-05-03 22:28:53 -0300

    feat: add functional memoization

--- wasm-build/wasm_bg.wasm.d.ts
+++ wasm-build/wasm_bg.wasm.d.ts
@@ -1,8 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function add_two_ints(a: number, b: number): number;
 export function fib(a: number): number;
+export function add_two_ints(a: number, b: number): number;
 export function greet(a: number, b: number): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,28 +1,21 @@
-use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::*;use fn_memo::{FnMemo, unsync, recur_fn::recur_fn};
+
+#[wasm_bindgen]
+pub fn fib(n: usize) -> usize {
+  let memo_fib = unsync::memoize(recur_fn(|fib, n: usize| {
+    println!("Evaluating {}", n);
+    if n <= 1 {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+  }));
+  return memo_fib.call(n);
+}
 
 #[wasm_bindgen]
 pub fn add_two_ints(a: u32, b: u32) -> u32 {
   a + b
-}
-
-#[wasm_bindgen]
-pub fn fib(n: usize) -> usize {
-    fn fib_memo(n: usize, memo: &mut [usize; 2]) -> usize {
-        let [a, b] = *memo;
-        let c = a + b;
-        if n == 0 {
-            c
-        } else {
-            *memo = [b, c];
-            fib_memo(n - 1, memo)
-        }
-    }
-
-    if n < 2 {
-        1
-    } else {
-        fib_memo(n - 2, &mut [1, 1])
-    }
 }
 
 #[wasm_bindgen]
