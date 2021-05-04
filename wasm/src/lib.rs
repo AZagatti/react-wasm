82065438822882ab13d313fd944caed698dfae6e
André Luis Zagatti Adorna <andre.zagatti1@gmail.com>
2021-05-03 21:38:06 -0300

    feat: update fib

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -6,11 +6,22 @@ pub fn add_two_ints(a: u32, b: u32) -> u32 {
 }
 
 #[wasm_bindgen]
-pub fn fib(n: u32) -> u32 {
-  if n == 0 || n == 1 {
-    return n;
-  }
-  fib(n - 1) + fib(n - 2)
+pub fn fib(number: usize) -> usize {
+    fn fib_memo(n: usize, memo: &mut [Option<usize>]) -> usize {
+        memo[n].unwrap_or_else(|| {
+            let result = {
+                if n > 1 {
+                    fib_memo(n - 1, memo) + fib_memo(n - 2, memo)
+                } else {
+                    1
+                }
+            };
+            memo[n] = Some(result);
+            result
+        })
+    }
+
+    fib_memo(number, &mut vec![None; number + 1])
 }
 
 #[wasm_bindgen]
